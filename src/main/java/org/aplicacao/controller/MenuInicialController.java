3e9b9e08aaeb7667f5318a021151a88b6a7bbb95
robertomalatestale <robertomalatestale@gmail.com>
2025-05-02 13:30:28 -0300

    feat: recreate some logic and made methods in Controller to introduce and show questions to register pet

--- src/main/java/org/aplicacao/controller/MenuInicialController.java
+++ src/main/java/org/aplicacao/controller/MenuInicialController.java
@@ -3,18 +3,29 @@ package org.aplicacao.controller;
 import org.aplicacao.model.MenuInicial;
 import org.aplicacao.view.MenuInicialView;
 
+import java.io.IOException;
+
 public class MenuInicialController {
     private MenuInicial menuInicial = new MenuInicial();
     private MenuInicialView menuInicialView = new MenuInicialView();
     private Integer answer = -1;
 
+
     public void initializeQuestionary(){
         while(answer>menuInicial.getPerguntas().size() || answer<0){
-            menuInicialView.showQuestions();
+            menuInicialView.showOptions();
             answer = menuInicialView.getUserAnswer();
             if(answer>menuInicial.getPerguntas().size() || answer<0){
                 System.out.println("Opção inválida, digite novamente");
             }
+        }
+    }
+
+    public void showQuestions() {
+        try {
+            menuInicialView.showQuestions();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/org/aplicacao/view/MenuInicialView.java
+++ src/main/java/org/aplicacao/view/MenuInicialView.java
@@ -1,17 +1,19 @@
 package org.aplicacao.view;
 
-import org.aplicacao.controller.InvalidInputException;
 import org.aplicacao.model.MenuInicial;
 import org.aplicacao.service.MenuInicialService;
+import org.aplicacao.service.ReaderService;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MenuInicialView {
     private MenuInicial menuInicial = new MenuInicial();
     private MenuInicialService menuInicialService = new MenuInicialService();
+    private ReaderService readerService = new ReaderService();
     private Scanner scanner = new Scanner(System.in);
 
-    public void showQuestions() {
+    public void showOptions() {
         System.out.println();
         for (int i = 0; i < menuInicial.getPerguntas().size(); i++) {
             System.out.println(menuInicial.getPerguntas().get(i));
@@ -24,6 +26,10 @@ public class MenuInicialView {
         Integer answer = scanner.nextInt();
         menuInicialService.menuInicialUserAnswerExceptionLogic(answer);
         return answer;
+    }
+
+    public void showQuestions() throws IOException {
+        System.out.println(readerService.readFormTxt());
     }
 
 }
