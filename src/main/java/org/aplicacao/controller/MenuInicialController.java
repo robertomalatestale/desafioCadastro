f63dc9db943af389baeb5a651a2eca365a9b2ab6
robertomalatestale <robertomalatestale@gmail.com>
2025-05-09 19:06:33 -0300

    feat: add a while logic to user input their option to select which functionality he wants to use

--- src/main/java/org/aplicacao/controller/MenuInicialController.java
+++ src/main/java/org/aplicacao/controller/MenuInicialController.java
@@ -1,6 +1,7 @@
 package org.aplicacao.controller;
 
 import org.aplicacao.model.MenuInicial;
+import org.aplicacao.service.PetCrudService;
 import org.aplicacao.view.MenuInicialView;
 
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.io.IOException;
 public class MenuInicialController {
     private MenuInicial menuInicial = new MenuInicial();
     private MenuInicialView menuInicialView = new MenuInicialView();
+    private PetCrudService petCrudService = new PetCrudService();
     private Integer answer = -1;
 
 
@@ -15,8 +17,13 @@ public class MenuInicialController {
         while(answer>menuInicial.getPerguntas().size() || answer<0){
             menuInicialView.showOptions();
             answer = menuInicialView.getUserAnswer();
-            if(answer>menuInicial.getPerguntas().size() || answer<0){
+            while(answer>menuInicial.getPerguntas().size() || answer<=0) {
                 System.out.println("Opção inválida, digite novamente");
+                answer = menuInicialView.getUserAnswer();
+            }
+            switch(answer){
+                case 6:
+                    petCrudService.finish();
             }
         }
     }
--- src/main/java/org/aplicacao/service/MenuInicialService.java
+++ src/main/java/org/aplicacao/service/MenuInicialService.java
@@ -7,11 +7,4 @@ public class MenuInicialService {
     private MenuInicial menuInicial = new MenuInicial();
 
     public void menuInicialUserAnswerExceptionLogic(Integer answer){
-        if(answer<=0){
-            throw new InvalidInputException("Insira um número correspondente a opção que deseja executar");
-        }
-        if(answer.getClass() != Integer.class){
-            throw new InvalidInputException("Insira um número inteiro correspondente a opção que deseja executar");
-        }
-    }
-}
+}}
