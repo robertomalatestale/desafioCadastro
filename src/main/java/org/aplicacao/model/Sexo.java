31dc9e4c192495d353d3c89903635117ab8cdec1
robertomalatestale <robertomalatestale@gmail.com>
2025-05-10 15:40:20 -0300

    feat: create a constructor for both Enum classes used in Pet class, also with aliases logic to identify user input when creating a pet

--- src/main/java/org/aplicacao/model/Sexo.java
+++ src/main/java/org/aplicacao/model/Sexo.java
@@ -1,6 +1,26 @@
 package org.aplicacao.model;
 
+import org.aplicacao.controller.InvalidInputException;
+
 public enum Sexo {
-    MASCULINO,
-    FEMININO
+    MASCULINO(new String[]{"m","masculino","macho","male"}),
+    FEMININO(new String[]{"f","feminino","femea","fêmea","female"});
+
+    private final String[] aliases;
+
+    Sexo(String[] aliases){
+        this.aliases = aliases;
+    }
+
+    public static Sexo identifyGender(String userInput){
+        for (Sexo sexo : Sexo.values()){
+            for (String alias : sexo.aliases){
+                if(alias.equalsIgnoreCase(userInput.trim())){
+                    return sexo;
+                }
+            }
+        }
+        throw new InvalidInputException("Escolha entre masculino e feminino");
+    }
+
 }
--- src/main/java/org/aplicacao/model/TipoPet.java
+++ src/main/java/org/aplicacao/model/TipoPet.java
@@ -1,6 +1,25 @@
 package org.aplicacao.model;
 
+import org.aplicacao.controller.InvalidInputException;
+
 public enum TipoPet {
-    CACHORRO,
-    GATO
+    CACHORRO(new String[]{"cão","cao","cachorro","dog"}),
+    GATO(new String[]{"gato","cat"});
+
+    private final String[] aliases;
+
+    TipoPet(String[] aliases){
+        this.aliases = aliases;
+    }
+
+    public static TipoPet identifyPetType(String userInput){
+        for(TipoPet tipoPet : TipoPet.values()){
+            for(String alias : tipoPet.aliases){
+                if(alias.equalsIgnoreCase(userInput.trim())){
+                    return tipoPet;
+                }
+            }
+        }
+        throw new InvalidInputException("Escolha entre cachorro e gato");
+    }
 }
